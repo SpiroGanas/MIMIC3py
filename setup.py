e1c82ed5c49d184c192e88d480ee83fb84dc8318
Spiro Ganas <spiroganas@gmail.com>
2017-08-27 11:43:13 -0400

    Added files to support distribution on pypi.

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'MIMIC3py',
+    packages = ['MIMIC3py'],
+    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'A Python library to load and analyze the MIMIC III Critical Care Database.',
+    author = 'Spiro Ganas',
+    author_email = 'spiroganas@gatech.edu',
+    url = 'https://github.com/SpiroGanas/MIMIC3py',
+    download_url = 'download link you saved',
+    keywords = ['MIMIC', 'Critical Care', 'Informatics'],
+    classifiers = [],
+)
