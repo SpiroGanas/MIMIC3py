712b031662959fbd73c9940b1916df9141e80603
Spiro Ganas <spiroganas@gmail.com>
2017-12-27 15:31:00 -0500

    updated setup.py to point to the second release

--- setup.py
+++ setup.py
@@ -3,12 +3,13 @@ from distutils.core import setup
 setup(
     name = 'MIMIC3py',
     packages = ['MIMIC3py'],
-    version = '0.1',  # Ideally should be same as your GitHub release tag varsion
+    version = '0.11',  # Ideally should be same as your GitHub release tag varsion
     description = 'A Python library to load and analyze the MIMIC III Critical Care Database.',
     author = 'Spiro Ganas',
     author_email = 'spiroganas@gatech.edu',
     url = 'https://github.com/SpiroGanas/MIMIC3py',
-    download_url = 'https://github.com/SpiroGanas/MIMIC3py/archive/0.1.tar.gz',
+    download_url = 'https://github.com/SpiroGanas/MIMIC3py/archive/v0.11.tar.gz',
     keywords = ['MIMIC', 'Critical Care', 'Informatics'],
     #classifiers = [],
 )
+
